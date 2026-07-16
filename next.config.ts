6ec7368b993247e99d1419f92d59d6dd19cdf1c5
c-jay69 <simonjc.mex@gmail.com>
2026-07-16 15:28:37 -0600

    lots of changes

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,13 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  output: "standalone",
-  /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      { protocol: 'http', hostname: '127.0.0.1' },
+      { protocol: 'http', hostname: 'localhost' },
+    ],
   },
-  reactStrictMode: false,
-};
+}
 
-export default nextConfig;
+export default nextConfig
